f20057e870878581c161327c8735cc47c70ddd5b
ishwarjare123-gif <ishwarjare123@gmail.com>
2026-03-26 14:33:35 +0530

    Add files via upload

--- Operator/Arithmetic.java
+++ Operator/Arithmetic.java
@@ -1,1 +1,12 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 6;
 
+        System.out.println("add="+(a+b));
+         System.out.println("substract="+(a-b));
+          System.out.println("multiply="+(a*b));
+           System.out.println("divide="+(a/b));
+            System.out.println("modulo(remainder)="+(a%b));
+    }
+}
