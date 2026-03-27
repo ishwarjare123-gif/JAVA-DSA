e20f0c73fa0b021f7f01baa707656eecd658d4cd
ishwarjare123-gif <ishwarjare123@gmail.com>
2026-03-28 00:41:04 +0530

    Add files via upload

---
+++ 03-Conditional Statements/_09switchex.java
@@ -0,0 +1,18 @@
+public class switchex{
+    public static void main(String[] args) {
+        
+        char ch ='b';
+
+        switch(ch){
+
+            case 'a':System.out.println("samosa");
+                      break;
+            case 'b':System.out.println("mango");
+                       break;
+            case 'c':System.out.println("banana");    
+                        break;
+             default :System.out.println("not availabe");  
+        }
+    }
+    
+}
