ab626593288e9b5db042364f9a8333844e81fa93
ishwarjare123-gif <ishwarjare123@gmail.com>
2026-03-28 00:37:34 +0530

    Add files via upload

---
+++ Conditional Statements/_02largest2number.java
@@ -0,0 +1,15 @@
+public class largest2number{                     
+    public static void main(String[] args){
+        int a =1;
+        int b =3;
+
+        if (a>=b){
+            System.out.println("a is largest number");
+        }
+        
+        else{
+            System.out.println("B is largest number");
+        }
+    }
+
+}
---
+++ Conditional Statements/_08studentresult.java
@@ -0,0 +1,7 @@
+public class studentresult {
+    public static void main(String[] args) {
+        int marks = 45;
+        String result = marks>=35?"pass":"fail";
+        System.out.println(result);
+    }
+}
