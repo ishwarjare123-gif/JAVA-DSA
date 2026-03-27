b09400b080b26f362580588c2aeb4537590d93d2
ishwarjare123-gif <ishwarjare123@gmail.com>
2026-03-27 23:56:12 +0530

    Add files via upload

---
+++ 01-Variables And Data Types/_2javabasic.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class javabasic {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      String str = sc.nextLine();
+      int x = sc.nextInt();
+      
+        System.out.println(x);
+        System.out.println(str);
+
+    }
+    
+}
---
+++ 01-Variables And Data Types/_9typeconversionorwiding.java
@@ -0,0 +1,14 @@
+// data type conversion byte>short>int>float>long>double
+import java.util.Scanner;
+
+public class typeconversionorwiding{
+public static void main (String args[]){
+
+//destination>source and computable
+Scanner sc = new Scanner(System.in);
+float a =sc.nextInt();
+System.out.println(a);
+}
+
+
+}
